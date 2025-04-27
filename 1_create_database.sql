9cc8237813d4d02cd7783bb7675fad40ea8ce9b3
maallem wissal <148656519+wissal988@users.noreply.github.com>
2025-04-27 15:23:19 +0000

    Update 1_create_database.sql

--- 1_create_database.sql
+++ 1_create_database.sql
@@ -1,15 +1,20 @@
 -- Create LabDatabase Database
-
-GO
-
--- Use LabDatabase Database
+CREATE DATABASE IF NOT EXISTS LabDatabase;
 USE LabDatabase;
-GO 
 
 -- Create Laboratoire Table
+CREATE TABLE Laboratoire (
+    CodeLab VARCHAR(4) PRIMARY KEY,
+    NomLab VARCHAR(100),
+    Datcreation DATE,
+    DatFin DATE,
+    Siteweb NVARCHAR(255)
+);
 
-
--- Create Chercheur Table 
-
-
-GO
+-- Create Chercheur Table
+CREATE TABLE Chercheur (
+    NumCh INT PRIMARY KEY,
+    NomCh VARCHAR(30),
+    CodeLab VARCHAR(4),
+    FOREIGN KEY (CodeLab) REFERENCES Laboratoire(CodeLab)
+);
